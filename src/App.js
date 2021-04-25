9f7de2cccc41db60568c87042805895646f7e603
Rokas <rookas.rudzenskas@gmail.com>
2021-04-25 16:34:56 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-        <h1>Hello, world! 🚀</h1>
+
 
         <div className="app__header">
             <img className="app_headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt=""/>
         </div>
 
-    {/*     Header    */}
+        <h1>Hello, world! 🚀</h1>
+
 
     {/*     Post    */}
     {/*     Post    */}
